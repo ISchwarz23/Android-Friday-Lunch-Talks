a95140097f075ad9b2302633b3c8e222f584ff4b
Ingo Schwarz <schwarz-ingo@web.de>
2015-11-11 18:16:25 +0100

    Implement login functionality

--- Session2/Implementation/app/src/main/java/org/lunchtalk/loginapp/LoginActivity.java
+++ Session2/Implementation/app/src/main/java/org/lunchtalk/loginapp/LoginActivity.java
@@ -1,13 +1,38 @@
 package org.lunchtalk.loginapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public static final String PARAM_USERNAME = "username";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText usernameField = (EditText) findViewById(R.id.username);
+        final EditText passwordField = (EditText) findViewById(R.id.password);
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                String enteredPassword = passwordField.getText().toString();
+                String enteredUsername = usernameField.getText().toString();
+                if (enteredPassword.isEmpty() || enteredUsername.isEmpty()) {
+                    // TODO: show some error
+                } else {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.putExtra(PARAM_USERNAME, enteredUsername);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
--- Session2/Implementation/app/src/main/java/org/lunchtalk/loginapp/MainActivity.java
+++ Session2/Implementation/app/src/main/java/org/lunchtalk/loginapp/MainActivity.java
@@ -2,6 +2,8 @@ package org.lunchtalk.loginapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String enteredUsername = getIntent().getStringExtra(LoginActivity.PARAM_USERNAME);
+
+        TextView usernameField = (TextView) findViewById(R.id.username);
+        usernameField.setText(enteredUsername);
     }
 }
