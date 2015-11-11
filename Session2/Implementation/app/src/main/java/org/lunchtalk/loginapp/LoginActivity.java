f32666407394206348ee36befaff736cfe8d6daa
Ingo Schwarz <schwarz-ingo@web.de>
2015-11-11 18:28:24 +0100

    Implement choosing username from contacts

--- Session2/Implementation/app/src/main/java/org/lunchtalk/loginapp/LoginActivity.java
+++ Session2/Implementation/app/src/main/java/org/lunchtalk/loginapp/LoginActivity.java
@@ -1,24 +1,38 @@
 package org.lunchtalk.loginapp;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Contacts;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Iterator;
+import java.util.Set;
 
 public class LoginActivity extends AppCompatActivity {
 
     public static final String PARAM_USERNAME = "username";
+    private static final int PICK_CONTACT_REQUEST_CODE = 10;
+
+    private EditText usernameField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        final EditText usernameField = (EditText) findViewById(R.id.username);
+        usernameField = (EditText) findViewById(R.id.username);
         final EditText passwordField = (EditText) findViewById(R.id.password);
         Button loginButton = (Button) findViewById(R.id.loginButton);
+        Button chooseUsernameButton = (Button) findViewById(R.id.chooseUsername);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,5 +48,28 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+
+        chooseUsernameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
+                startActivityForResult(intent, PICK_CONTACT_REQUEST_CODE);
+            }
+        });
     }
+
+    public void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK && reqCode == PICK_CONTACT_REQUEST_CODE) {
+            Uri contactUri = data.getData();
+            Cursor cursor = getContentResolver().query(contactUri, null, null, null, null);
+            cursor.moveToFirst();
+            int nameColumn = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+            String chosenUsername = cursor.getString(nameColumn);
+            usernameField.setText(chosenUsername);
+        }
+    }
+
 }
