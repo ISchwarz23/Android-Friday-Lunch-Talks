ca50642f70fdd52cfea951b568dfd3d2233fdd05
Ingo Schwarz <schwarz-ingo@web.de>
2016-01-09 13:59:39 +0100

    Add custom attributes

--- Session4/Implementation/app/src/main/java/org/lunchtalk/compoundview/CounterView.java
+++ Session4/Implementation/app/src/main/java/org/lunchtalk/compoundview/CounterView.java
@@ -1,7 +1,9 @@
 package org.lunchtalk.compoundview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -13,23 +15,27 @@ import android.widget.TextView;
  */
 public class CounterView extends LinearLayout {
 
+    private static final int DEFAULT_INITIAL_COUNT = 0;
+    private static final int DEFAULT_COUNTER_STEP_SIZE = 1;
+
     private TextView counterTextView;
     private Button decrementButton;
     private Button incrementButton;
 
-    private int count = 0;
-    private int counterStep = 1;
+    private int count;
+    private int counterStep;
 
     public CounterView(final Context context) {
         this(context, null);
     }
 
     public CounterView(final Context context, final AttributeSet attrs) {
-        this(context, null, 0);
+        this(context, attrs, 0);
     }
 
     public CounterView(final Context context, final AttributeSet attrs, final int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initializeAttributes(attrs);
         initializeViews();
         initializeListeners();
     }
@@ -42,33 +48,58 @@ public class CounterView extends LinearLayout {
         this.count = count;
     }
 
-    public int getCounterStep() {
+    public int getCounterStepSize() {
         return counterStep;
     }
 
-    public void setCounterStep(final int counterStep) {
+    public void setCounterStepSize(final int counterStep) {
         this.counterStep = counterStep;
+    }
+
+    public void incrementCounter() {
+        updateCounter(counterStep);
+    }
+
+    public void decrementCounter() {
+        updateCounter(-counterStep);
+    }
+
+    private void updateCounter(int counterStep) {
+        count += counterStep;
+        showCountInTextView();
+    }
+
+    private void showCountInTextView() {
+        counterTextView.setText(String.valueOf(count));
+    }
+
+    private void initializeAttributes(final AttributeSet attributes) {
+        final TypedArray styledAttributes = getContext().obtainStyledAttributes(attributes, R.styleable.CounterView);
+        count = styledAttributes.getInteger(R.styleable.CounterView_initialCount, DEFAULT_INITIAL_COUNT);
+        counterStep = styledAttributes.getInteger(R.styleable.CounterView_countingStepSize, DEFAULT_COUNTER_STEP_SIZE);
+        styledAttributes.recycle();
     }
 
     private void initializeViews() {
         LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View rootView = inflater.inflate(R.layout.counter_view, this, true);
-        counterTextView = (TextView) rootView.findViewById(R.id.counterTextView);
         decrementButton = (Button) rootView.findViewById(R.id.decrementButton);
         incrementButton = (Button) rootView.findViewById(R.id.incrementButton);
+        counterTextView = (TextView) rootView.findViewById(R.id.counterTextView);
+        showCountInTextView();
     }
 
     private void initializeListeners() {
         incrementButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(final View v) {
-                count += counterStep;
+                incrementCounter();
             }
         });
         decrementButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(final View v) {
-                count -= counterStep;
+                decrementCounter();
             }
         });
     }
