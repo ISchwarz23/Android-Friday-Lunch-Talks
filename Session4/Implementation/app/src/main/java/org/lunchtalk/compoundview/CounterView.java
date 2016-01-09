c3a0bb1527d7b234b7a595e301252c575866f62c
Ingo Schwarz <schwarz-ingo@web.de>
2016-01-09 14:04:18 +0100

    Do some aesthetic improvements

--- Session4/Implementation/app/src/main/java/org/lunchtalk/compoundview/CounterView.java
+++ Session4/Implementation/app/src/main/java/org/lunchtalk/compoundview/CounterView.java
@@ -3,7 +3,6 @@ package org.lunchtalk.compoundview;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
