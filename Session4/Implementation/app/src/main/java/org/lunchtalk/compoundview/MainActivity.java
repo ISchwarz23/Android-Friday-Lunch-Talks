31b29af158869ead44d252e49d520505a646f9c7
Ingo Schwarz <schwarz-ingo@web.de>
2016-01-14 17:33:35 +0100

    Add code usage example of CounterView

--- Session4/Implementation/app/src/main/java/org/lunchtalk/compoundview/MainActivity.java
+++ Session4/Implementation/app/src/main/java/org/lunchtalk/compoundview/MainActivity.java
@@ -2,6 +2,9 @@ package org.lunchtalk.compoundview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final CounterView counterView = (CounterView) findViewById(R.id.counterView);
+        final Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                String toastMessage = "The count is " + counterView.getCount();
+                Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
